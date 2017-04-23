450e2e95430d53f5aa3c883c473634e03835072b
RyeSky <1522316772@qq.com>
2017-04-23 15:50:58 +0800

    ''

--- lover v1.0.1/code/android/app/src/main/java/com/skye/lover/mvp/presenter/BasePresenterEmptyImpl.java
+++
@@ -1,9 +0,0 @@
-package com.skye.lover.mvp.presenter;
-
-import com.skye.lover.mvp.view.BaseView;
-
-/**
- * 主导器基类空实现
- */
-public class BasePresenterEmptyImpl extends BasePresenter<BaseView> {
-}
--- lover v1.0.1/code/android/app/src/main/java/com/skye/lover/view/exrecyclerview/OnBottomListener.java
+++
@@ -1,8 +0,0 @@
-package com.skye.lover.view.exrecyclerview;
-
-/**
- * recyclerview滑动到底部时触发
- */
-public interface OnBottomListener {
-    void onBottom();
-}
